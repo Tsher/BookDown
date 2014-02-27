d5975aadac530ceaa6e8495dcecbd908da6e4a90
MyBase <262747531@qq.com>
2014-02-27 14:14:03 +0800

    add

---
+++ 20140227/深入浅出Node_js（八）：Connect模块解析（之二）静态文件中间件_files/infoqcn.js
@@ -0,0 +1,4 @@
+//此文件是给InfoQ中文站专用，勿删。修改请联系Charles
+
+$(".text_info .random_links").before('<div style="margin-bottom:5px"><a href="http://www.diaochapai.com/survey869696" target="_blank"><img src="http://www.qconbeijing.com/images/20140216_survey.jpg" /></a></div>')
+console.log('请关注infoq微信帐号:infoqchina');
